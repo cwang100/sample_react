c37fae1631f652acc0185c8bedbf291312921dea
cwang <cwang100@illinois.edu>
2018-01-23 21:41:52 +0800

    add textbox element

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
 
 class Button extends Component {
-	constructor(props) {
-    super(props);
-    this.state = { x: 0, y: 0 };
-  }
-  
   render() {
     return (
       <button 
--- src/index.js
+++ src/index.js
@@ -3,28 +3,13 @@ import { Provider, connect } from 'react-redux';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware  } from 'redux';
 import './index.css';
-import App from './App';
+import App1 from './App';
 import registerServiceWorker from './registerServiceWorker';
 import {reducer} from './reducer/SampleReducer.js';
-import {getData} from './actions/SampleActions.js';
 import React, { Component } from 'react';
 import thunk from 'redux-thunk';
 
 let store = createStore(reducer, applyMiddleware(thunk));
-
-function mapStateToProps(state) {  
-    return { text: state.text }  
-}  
-  
-//映射Redux actions到组件的属性  
-function mapDispatchToProps(dispatch){  
-    return {  
-        onButtonClick:()=>dispatch(getData)
-    }  
-}  
-  
-//连接组件  
-var App1 = connect(mapStateToProps, mapDispatchToProps)(App)  
   
 //渲染组件  
 ReactDOM.render(  
