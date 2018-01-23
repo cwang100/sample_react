f6e77cac5b35ee135781f2315f61130e04f38af3
Xiaoyi Chen <xchen84@illinois.edu>
2018-01-23 22:01:49 +0800

    add table component

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,37 +1,15 @@
 import React, { Component } from 'react';
 
-
-//usage: <StaffItemPanel items={rawData} />
-class Table extends Component {
-	constructor(props) {
-    super(props);
-  }
-
-  let header = [];
-  let items = [];
-
-
-  this.props.header.forEach(head => {
-          items.push(<TableItem key={item.key} item={item}/>);
-      });
-  if(this.props.items.length == 0) {
-      items.push(<tr><th colSpan=this.props.header.length className="tempEmpty">Nothing to show</th></tr>);
-  }else {
-      this.props.items.forEach(item => {
-          items.push(<StaffItem key={item.key} item={item}/>);
-      });
-  }
-  
+class Table extends Component {  
   render() {
     return (
-      <table>
-        <thead>
-          {header}
-        </thead>
-        <tbody>{items}</tbody>
+      <table 
+        className="table table-striped"       
+      >
+        { this.props.children }
       </table>
     );
   }
 }
 
-export default Button;
+export default Table;
