513d8c1692188035d9aefec490d8ee99d0211006
cwang <cwang100@illinois.edu>
2018-01-29 22:48:09 +0800

    add loading screen

--- src/reducer/SampleReducer.js
+++ src/reducer/SampleReducer.js
@@ -9,27 +9,37 @@ const initialState = {
 export const reducer = (state = initialState, action) => {
     let newState = initialState;
     switch (action.type) {   
+        case 'LOADING':
+            newState = _.assign({}, state, {
+                loading: true
+            });
+            
+            return newState;
+
         case 'BUTTON_CLICK': 
         	let fullData = action.data;
         	let eventIds = fullData.event_ids;
-            newState = _.assign({}, initialState, {
+            newState = _.assign({}, state, {
                 events: fullData.events,
                 eventIDs: eventIds,
                 contract_groups: fullData.contract_groups,
-                contracts: fullData.contracts
+                contracts: fullData.contracts,
+                loading: false
             });
             
 		    return newState;
 
 		case 'CLICK_INTO_EVENT':
-            newState = _.assign({}, initialState, {
-                eventDetails: action.data
+            newState = _.assign({}, state, {
+                eventDetails: action.data,
+                loading: false
             });
         	return newState;
         
         case 'POPULAR_EVENTS':
-            newState = _.assign({}, initialState, {
-                popular: action.data
+            newState = _.assign({}, state, {
+                popular: action.data,
+                loading: false
             });
             return newState;
         default:  
